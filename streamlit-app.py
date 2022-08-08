2347e59f6c3a27c323260a505c66c01e648f766d
sowmyac66 <110240068+sowmyac66@users.noreply.github.com>
2022-08-08 12:18:36 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,6 +1,6 @@
 
 import streamlit
-streamlit.title('Hello world')
+streamlit.title('Breakfast Favorites')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')

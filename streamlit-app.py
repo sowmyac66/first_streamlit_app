48b27771722f9c23ce564bed1ca2aa82a3760c8d
sowmyac66 <110240068+sowmyac66@users.noreply.github.com>
2022-09-01 16:21:46 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -40,7 +40,7 @@ streamlit.stop()
 
 streamlit.header("The fruit load list contains")
 def get_fruit_load_list():
-    with my_cnx_cursor as my_cur:
+    with my_cnx.cursor as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return mycur.fetchall()
 
